4ab6f098a065327eeb48b37bda002b1ece129600
Kushul Soomaree <kushul@lsl.digital>
2023-03-02 21:16:42 +0400

    added assets for scss

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,7 +7,16 @@ export default defineNuxtConfig({
             apiBase: '/api'
         }
     },
+    app: {
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;0,800;1,300;1,400;1,600;1,800&display=swap' },
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;700&display=swap' }]
+        }
+    },
     css: [
+
         '@/assets/scss/main.scss',
     ],
 })
