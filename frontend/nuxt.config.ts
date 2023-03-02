4a7e42548dbd1b49b658bb5185b7d84ffedbf057
Kushul Soomaree <kushul@lsl.digital>
2023-03-02 21:31:19 +0400

    add page transition

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+
     runtimeConfig: {
         // The private keys which are only available server-side
         apiSecret: '123',
@@ -23,7 +24,8 @@ export default defineNuxtConfig({
                 { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
                 { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;0,800;1,300;1,400;1,600;1,800&display=swap' },
                 { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;700&display=swap' }]
-        }
+        },
+        pageTransition: { name: 'page', mode: 'out-in' }
     },
     css: [
 
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,14 @@
 <script setup>
 const runtimeConfig = useRuntimeConfig()
 </script>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
